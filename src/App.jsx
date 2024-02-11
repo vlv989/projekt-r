d5b5097628ee02cb01070bc22107033f41a2d3c6
vlv989 <konan12@tlen.pl>
2024-02-11 04:26:33 +0100

    zad2a

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import AddProducts from "./components/zaliczenie/AddProducts/AddProducts";
 import ProductsFilters from "./components/zaliczenie/ProductsFilters/ProductsFilters";
 import ProductsList from "./components/zaliczenie/ProductsList/ProductsList";
 import ShopingList from "./components/zaliczenie/ShopingList/ShopingList";
+import produkty from "./common/consts/produkty";
 
 function App() {
+  const [filteredProducts, setFilteredProducts] = useState(produkty);
   const [shoppingProducts, setShoppingProducts] = useState([]);
 
   const addProductToCart = (product) => {
@@ -16,12 +18,31 @@ function App() {
     setShoppingProducts(shoppingProducts.filter((el) => el.id !== id));
   };
 
+  const filterProducts = (data) => {
+    let filterByData = produkty.filter((el) =>
+      el.nazwa.toLowerCase().startsWith(data.nazwa.toLowerCase())
+    );
+
+    data.kategoria !== "All" &&
+      (filterByData = filterByData.filter(
+        (el) => el.kategoria.toLowerCase() === data.kategoria
+      ));
+
+    data.produktSpozywczy &&
+      (filterByData = filterByData.filter((el) => el.produktSpozywczy));
+
+    setFilteredProducts(filterByData);
+  };
+
   return (
     <div className={styles.appWrapper}>
       <AddProducts />
-      <ProductsFilters />
+      <ProductsFilters filterProducts={filterProducts} />
       <div className={styles.columnsWrapper}>
-        <ProductsList addProductToCart={addProductToCart} />
+        <ProductsList
+          products={filteredProducts}
+          addProductToCart={addProductToCart}
+        />
         <ShopingList
           products={shoppingProducts}
           removeProductFromCart={removeProductFromCart}
